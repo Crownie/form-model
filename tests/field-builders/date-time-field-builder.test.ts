2bd9e5a37ae4e4ddc8a103fe5c70a19ca2692d91
Tobi <stevehappyboy@rocketmail.com>
2020-06-21 09:16:25 +0100

    test null date

--- tests/field-builders/date-time-field-builder.test.ts
+++ tests/field-builders/date-time-field-builder.test.ts
@@ -1,11 +1,15 @@
 import {DateTimeFieldBuilder} from '../../src/field-builders';
 
-it('DateTimeFieldBuilder', () => {
+it('DateTimeFieldBuilder', async () => {
   const field = new DateTimeFieldBuilder('Date').build();
 
   const field2 = new DateTimeFieldBuilder('Date').dateOnly('MM/YYYY').build();
 
   const field3 = new DateTimeFieldBuilder('Time').timeOnly('hh:mm').build();
+
+  // test null value for date
+  const schema = field.getSchema();
+  await expect(schema.validate(null)).resolves.toEqual(null);
 
   expect(field).toMatchSnapshot();
   expect(field2).toMatchSnapshot();
